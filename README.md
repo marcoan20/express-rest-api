e84214f18f1a5e800ba88ec266eaa38036e281d4
Marco Antônio <marcopierozan20@gmail.com>
2023-08-26 00:21:00 -0300

    :sparkles: feat: add account creation

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # Projeto de API para um banco fictício
 
 ### Funcionalidades:
-- [] Criar uma conta.
-- [] Tirar extrato.
-- [] Depósitos.
-- [] Saques.
-- [] Tirar o extrato por data.
-- [] Atualizar dados da conta.
-- [] Deletar conta.
+- -[x] Criar uma conta.
+- -[] Tirar extrato.
+- -[] Depósitos.
+- -[] Saques.
+- -[] Tirar o extrato por data.
+- -[] Atualizar dados da conta.
+- -[] Deletar conta.
--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,44 @@
-console.log("HELLO WORLD!");
+import { Request, Response } from "express";
+
+const express = require('express');
+const { v4: uuidv4 } = require('uuid');
+
+const PORT = 3333;
+
+const app = express();
+
+interface ICostumer {
+  id: string;
+  name: string;
+  cpf: string;
+  statement: [];
+}
+
+const costumers: ICostumer[] = [];
+
+app.use(express.json());
+
+app.post('/account', (req: Request, res: Response) => {
+  const {cpf, name} = req.body;
+
+  const costumerAlreadyExists = costumers.some(
+    (costumer) => costumer.cpf === cpf
+  );
+
+  if(costumerAlreadyExists) {
+    return res.status(400).json({error: "Costumer already exists!"});
+  }
+
+  costumers.push({
+    id: uuidv4(),
+    name,
+    cpf,
+    statement: []
+  });
+
+  return res.status(201).send();
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
